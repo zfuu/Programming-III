e7555e24794dee2b09dbadd51cba4068aca715d7
Ziyu Fu <fuziyu0730@gmail.com>
2021-10-17 23:22:23 +0900

    Week 1

---
+++ Week 1/prob1A.c
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+
+int main(int argc, char* argv[]){
+    int i, j;
+
+
+    // for (i = 1; i < argc; i++){
+    for (i = argc-1; i >= 1; --i){
+        for (j = 0; argv[i][j] != '\0'; ++j);
+        for (j = j; j >= 0; --j){
+            printf("%c", argv[i][j]);
+        } 
+        printf("\n");
+    }
+}
---
+++ Week 1/prob1B.c
@@ -0,0 +1,32 @@
+#include<stdio.h> 
+#include <string.h>
+
+int main(int argc, char* argv[]){
+    char count[128] = {0};
+    int longestLength = 0;
+    int longestIndex = 0;
+    int mostFrequentLetter = 0;
+    int frequencyCount = 0;
+
+    for (int i = 1; i <= argc-1; ++i){
+        if (strlen(argv[i]) > longestLength){
+            longestLength = strlen(argv[i]);
+            longestIndex = i;
+        }
+    }
+
+    for (int j = 0; argv[longestIndex][j] != '\0'; j++){
+        count[argv[longestIndex][j]]++;
+    }
+
+    for (int k = 0; k <= 128; k++){
+        if (count[k] > frequencyCount){
+            frequencyCount = count[k];
+            mostFrequentLetter = k;
+        }
+    }
+
+    printf("%c, %d", mostFrequentLetter, frequencyCount);
+
+}
+
