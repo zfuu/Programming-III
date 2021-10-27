35221c0c698483d3e54f558157060ace44f74885
Ziyu Fu <fuziyu0730@gmail.com>
2021-10-27 18:50:09 +0900

    Week 2
    2B

--- Week 2/prob2B.c
+++ Week 2/prob2B.c
@@ -4,11 +4,14 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+#include <ctype.h>
 
 #define N 4096
 
 int main(int argc, char *argv[]){
     char buf[N];
+    char c;
     int fd, i, j, n, k=0, count=0;
     int len = strlen(argv[1]);
     for (i = 2; i < argc; i++) {
@@ -16,12 +19,27 @@ int main(int argc, char *argv[]){
             fprintf(stderr, "cannot open %s\n", argv[i]);
             continue;
         }
+ 
         while ((n = read(fd, buf, N)) > 0) {
+            // printf("%s", buf);
             for (j = 0; j < n; j++) {
                 // state diagram to count the appearance of argv[1]
-            }
+                c = tolower(buf[j]);
+                // printf("%c", buf[j]);
+                if (c == argv[1][k]){
+                    // printf("%c", c);
+                    k += 1;
+                    if (k == len && !isalpha(buf[j+1])){
+                        count ++;
+                        k = 0;
+                    }
+                }else if (c != argv[1][k]){
+                    k = 0;
+                }  
+            }            
         }
         close(fd);
     }
+
     printf("%d\n", count);
 }
