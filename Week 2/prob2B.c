452934eb144b683e247999cdddd9c700db1c84aa
Ziyu Fu <fuziyu0730@gmail.com>
2021-10-28 01:45:52 +0900

    Week 2
    2B doesnt work yet

--- Week 2/prob2B.c
+++ Week 2/prob2B.c
@@ -12,8 +12,15 @@
 int main(int argc, char *argv[]){
     char buf[N];
     char c;
-    int fd, i, j, n, k=0, count=0;
+    int fd, i, j, n, a, k=0, count=0;
     int len = strlen(argv[1]);
+    char target[len+1];
+    for ( a = 0; a < len; a++){
+        target[a] = argv[1][a];
+    }
+    target[len] = ' ';
+    // printf("[%s]", target);
+
     for (i = 2; i < argc; i++) {
         if ((fd = open(argv[i], O_RDONLY)) < 0) {
             fprintf(stderr, "cannot open %s\n", argv[i]);
@@ -21,19 +28,31 @@ int main(int argc, char *argv[]){
         }
  
         while ((n = read(fd, buf, N)) > 0) {
-            // printf("%s", buf);
+            // printf("\n");
+            // printf("%d\n",n);
+            // printf("%s\n", buf);
             for (j = 0; j < n; j++) {
                 // state diagram to count the appearance of argv[1]
                 c = tolower(buf[j]);
+                // printf("%d%c ", k,c);
+                // printf("%d[%c?%c]/%d ", j,c,argv[1][k],k);
+                
                 // printf("%c", buf[j]);
-                if (c == argv[1][k]){
+                if (c == target[k]){
                     // printf("%c", c);
                     k += 1;
-                    if (k == len && !isalpha(buf[j+1])){
-                        count ++;
-                        k = 0;
-                    }
-                }else if (c != argv[1][k]){
+                    // if ((k == len && !isalpha(buf[j+1])) || j==N && k>0){
+                    // if (j != n-1){
+                        if (k == len+1){
+                            count ++;
+                            k = 0;
+                        }
+                    
+                    // }
+                    
+
+
+                }else if (c != target[k]){
                     k = 0;
                 }  
             }            
@@ -41,5 +60,6 @@ int main(int argc, char *argv[]){
         close(fd);
     }
 
+    // printf("\n");
     printf("%d\n", count);
 }
